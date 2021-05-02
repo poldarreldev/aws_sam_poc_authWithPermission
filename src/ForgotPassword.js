f17535584761a7b64a1f18a4b12cb148e33901a7
poldarrel.dev <poldarrel.dev@gmail.com>
2021-05-01 19:29:24 -0500

    initial commit

---
+++ src/ForgotPassword.js
@@ -0,0 +1,20 @@
+/* src/ForgotPassword.js */
+import React from 'react'
+import Button from './Button'
+import { styles } from './Form'
+
+function ForgotPassword(props) {
+  return (
+    <div style={styles.container}>
+      <input
+        name='username'
+        placeholder='Username'
+        onChange={e => {e.persist();props.updateFormState(e)}}
+        style={styles.input}
+      />
+      <Button onClick={props.forgotPassword} title="Reset password" />
+    </div>
+  )
+}
+
+export default ForgotPassword
